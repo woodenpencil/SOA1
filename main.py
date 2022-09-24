35631beb9e2cf9094ec7583545987a537c241b4a
Anton Antonov <60038254+woodenpencil@users.noreply.github.com>
2022-09-24 20:26:18 +0300

    v 0.3

--- main.py
+++ main.py
@@ -23,38 +23,43 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
 
-class Carbrands(db.Model):
-    __tablename__ = 'carbrands'
-    brand_id = db.Column(db.Integer, primary_key=True)
-    brand_name = db.Column(db.String(250))
+class Regions(db.Model):
+    __tablename__ = 'regions'
+    region_id = db.Column(db.Integer, primary_key=True)
+    region_name = db.Column(db.String(250))
 
     def __repr__(self):
-        return '\n brand_id: {0} brand_name: {1}'.format(self.brand_id, self.brand_name)
+        return '\n region_id: {0} region_name: {1}'.format(self.region_id, self.region_name)
 
     def __str__(self):
-        return '\n brand_id: {0} brand_name: {1}'.format(self.brand_id, self.brand_name)
+        return '\n region_id: {0} region_name: {1}'.format(self.region_id, self.region_name)
 
 
-class Carmodels(db.Model):
-    __tablename__ = 'carmodels'
-    model_id = db.Column(db.Integer, primary_key=True)
-    brand_id = db.Column(db.Integer)
-    car_model = db.Column(db.String(250))
+class Districts(db.Model):
+    __tablename__ = 'districts'
+    district_id = db.Column(db.Integer, primary_key=True)
+    region_id = db.Column(db.Integer)
+    region_name = db.Column(db.String(250))
 
     def __repr__(self):
-        return '\n model_id: {0} brand_id: {1} car_model: {2}'.format(self.model_id, self.brand_id, self.car_model)
+        return '\n district_id: {0} region_id: {1} region_name: {2}'.format(self.district_id, self.region_id, self.region_name)
 
     def __str__(self):
-        return '\n model_id: {0} brand_id: {1} car_model: {2}'.format(self.model_id, self.brand_id, self.car_model)
+        return '\n district_id: {0} region_id: {1} region_name: {2}'.format(self.district_id, self.region_id, self.region_name)
 
 class Towns(db.Model):
-    __tablename__ = 'towns'
+    __tablename__ = 'town'
     town_id = db.Column(db.Integer, primary_key=True)
     town_name= db.Column(db.String(250))
-    carmodel_id = db.Column(db.Integer)
-    def __init__(self, carmodel_id, town_name):
+    district_id = db.Column(db.Integer)
+    town_type = db.Column(db.String(250))
+    population = db.Column(db.Integer)
+
+    def __init__(self, district_id, town_name, town_type, population):
         self.town_name=town_name
-        self.carmodel_id=carmodel_id
+        self.district_id=district_id
+        self.town_type = town_type
+        self.population = population
 
 
 def get_dropdown_values():
@@ -75,21 +80,21 @@ could be defined globally
     # {'Toyota': ['Tercel', 'Prius'],
     #  'Honda': ['Accord', 'Brio']}
 
-    carbrands = Carbrands.query.all()
+    regions = Regions.query.all()
     # Create an empty dictionary
     myDict = {}
-    for p in carbrands:
+    for p in regions:
 
-        key = p.brand_name
-        brand_id = p.brand_id
+        key = p.region_name
+        region_id = p.region_id
 
         # Select all car models that belong to a car brand
-        q = Carmodels.query.filter_by(brand_id=brand_id).all()
+        q = Districts.query.filter_by(region_id=region_id).all()
 
         # build the structure (lst_c) that includes the names of the car models that belong to the car brand
         lst_c = []
         for c in q:
-            lst_c.append(c.car_model)
+            lst_c.append(c.region_name)
         myDict[key] = lst_c
 
     class_entry_relations = myDict
@@ -101,11 +106,6 @@ could be defined globally
 def update_dropdown():
     # the value of the first dropdown (selected by the user)
     selected_class = request.args.get('selected_class', type=str)
-    #selected_entry = request.args.get('selected_enrty', type=str)
-    #print(selected_class)
-    #print(selected_entry)
-    #print(selected_class) - > 'Mitsubishi'
-    # get values for the second dropdown
     updated_values = get_dropdown_values()[selected_class]
 
     # create the value sin the dropdown as a html string
@@ -114,7 +114,7 @@ def update_dropdown():
         html_string_selected += '<option value="{}">{}</option>'.format(entry, entry)
 
     return jsonify(html_string_selected=html_string_selected)
-
+'''
 @app.route('/_update_towns')
 def update_towns():
     # the value of the second dropdown (selected by the user)
@@ -129,7 +129,7 @@ def update_towns():
     #    html_string_selected += '<option value="{}">{}</option>'.format(entry, entry)
 
     return jsonify(html_string_towns="")
-
+'''
 
 @app.route('/_process_data')
 def process_data():
@@ -138,8 +138,8 @@ def process_data():
     selected_towns = request.args.get('selected_town', type=str)
     #print(selected_class)
     #print(selected_entry)
-    sel = Carmodels.query.filter_by(car_model=selected_entry).first().model_id
-    selected_towns = Towns.query.with_entities(Towns.town_name, Towns.carmodel_id).filter_by(carmodel_id=sel)
+    sel = Districts.query.filter_by(region_name=selected_entry).first().district_id
+    selected_towns = Towns.query.with_entities(Towns.town_name, Towns.district_id, Towns.town_type, Towns.population).filter_by(district_id=sel)
     #print(selected_towns.all())
     res = selected_towns.all()
     #print(res)
@@ -150,9 +150,9 @@ def process_data():
         res[i] = t
     #dis_id = map(lambda x: x[1]=selected_entry, res)
     #res = list(res)
-    #print(res)
-    if len(res)>1:
-        res = ';'.join(res)
+    print(res)
+    #if len(res)>1:
+    #    res = ';'.join(res)
     # process the two selected values here and return the response; here we just create a dummy string
 
     return jsonify(
@@ -182,7 +182,7 @@ def addtown():
     if request.method == 'POST':
         district = request.form['district']
         town = request.form['town']
-        sel = Carmodels.query.filter_by(car_model=district)
+        sel = Districts.query.filter_by(region_name=district)
 
         if not district:
             flash('Введите название района!')
@@ -191,7 +191,7 @@ def addtown():
         elif not town:
             flash('Введите название населенного пункта')
         else:
-            newtown = Towns(sel.first().model_id, town)
+            newtown = Towns(sel.first().district_id, town)
             db.session.add(newtown)
             db.session.commit()
             return redirect(url_for('index'))
@@ -207,7 +207,7 @@ def deletetown():
     if request.method == 'POST':
         district = request.form['district']
         town = request.form['town']
-        sel = Carmodels.query.filter_by(car_model=district)
+        sel = Districts.query.filter_by(region_name=district)
         if not district:
             flash('Введите название района!')
         elif sel.first()==None:
@@ -232,11 +232,11 @@ def edittown():
         population = request.form['population']
         newtype = request.form['type']
         population = int(population)
-        print(newtown)
-        print(population)
-        print(newtype)
+        #print(newtown)
+        #print(population)
+        #print(newtype)
 
-        sel = Carmodels.query.filter_by(car_model=district)
+        sel = Districts.query.filter_by(region_name=district)
         newtown = newtown[1:]
         newtype = newtype[1:]
         if not district:
@@ -248,12 +248,12 @@ def edittown():
         else:
             town = town[1:]
             sel = Towns.query.filter_by(town_name=town).first().town_id
-            sel_dis = Towns.query.filter_by(town_name=town).first().carmodel_id
-            selected_towns = Towns.query.with_entities(Towns.town_name, Towns.carmodel_id).filter_by(town_id=sel).first()
-            #Towns.query.filter_by(town_name=town).delete()
+            sel_dis = Towns.query.filter_by(town_name=town).first().district_id
+            selected_towns = Towns.query.with_entities(Towns.town_name, Towns.district_id).filter_by(town_id=sel).first()
+            Towns.query.filter_by(town_name=town).delete()
             print(selected_towns)
             #print(newtown)
-            newt = Towns(selected_towns[1], newtown)
+            newt = Towns(selected_towns[1], newtown, newtype, population)
             db.session.add(newt)
             db.session.commit()
             return redirect(url_for('index'))
